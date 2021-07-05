901d6f71f83a91007b269f96bbf4573ee55f9374
Ryan Adhitama Putra <ryanadhitama2@gmail.com>
2021-07-05 14:09:19 +0800

    show detail post

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -4,6 +4,7 @@ const models = require('../models/index')
 class PostController {
     constructor(){}
 
+    // INDEX
     async index(req, res, next) {
         try {
             const posts = await models.posts.findAll({
@@ -26,6 +27,44 @@ class PostController {
                 return res.json({
                     'success': true,
                     'messages': 'Data is empty',
+                    'data': {} 
+                });
+            }
+       } catch (error) {
+            return res.status(400).json({
+                'success': false,
+                'messages': error.message
+            });
+       }
+    }
+
+    // SHOW
+    async show(req, res, next) {
+        try {
+            const id = req.params.postId
+            const post = await models.posts.findOne({
+                where: {
+                    id: id
+                },  
+                include: [
+                    {
+                        model: models.categories
+                    },
+                    {
+                        model: models.tags
+                    }
+                ]
+            })
+            if (post) {
+                return res.json({
+                    'success': true,
+                    'message': 'Data found',
+                    'data': post
+                });
+            } else {
+                return res.status(404).json({
+                    'success': true,
+                    'messages': 'Data not found',
                     'data': {} 
                 });
             }
--- router/index.js
+++ router/index.js
@@ -2,5 +2,6 @@ const router = require('express').Router()
 const PostController = require('../controllers/post.controller.js')
 
 router.get('/posts', PostController.index)
+router.get('/posts/:postId', PostController.show)
 
 module.exports = router
